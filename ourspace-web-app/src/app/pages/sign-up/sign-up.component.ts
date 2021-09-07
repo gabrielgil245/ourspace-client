41f143a8a0c926d42220af09bd9cb2f918eab094
Jack Gildea <jack.k.gildea@gmail.com>
2021-09-07 18:57:21 -0400

    sign up page

--- ourspace-web-app/src/app/pages/sign-up/sign-up.component.ts
+++ ourspace-web-app/src/app/pages/sign-up/sign-up.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
+  _username: String = "";
+  _password: String = "";
+  _first_name: String = "";
+  _last_name: String = "";
+  _email: String = "";
+  _birthday: Date | undefined;
+  _about_me: String | undefined;
+  _profile_pic: String | undefined;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  displayInfo(){
+    console.log(this._username);
+    console.log(this._password);
+    console.log(this._first_name);
+    console.log(this._last_name);
+    console.log(this._email);
+    console.log(this._birthday);
+    console.log(this._about_me);
+    
+  }
 }
