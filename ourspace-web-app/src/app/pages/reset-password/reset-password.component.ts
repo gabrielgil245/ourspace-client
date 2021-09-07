b7205d2e2ad81bac3120045bb6885a50b3c7fc02
Joe <deglm006@morris.umn.edu>
2021-09-07 18:14:17 -0500

    Create structure of reset password form

--- ourspace-web-app/src/app/pages/reset-password/reset-password.component.ts
+++ ourspace-web-app/src/app/pages/reset-password/reset-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reset-password',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reset-password.component.css']
 })
 export class ResetPasswordComponent implements OnInit {
+  resetPasswordForm = new FormGroup({
+    password: new FormControl('',Validators.required),
+    confirmPassword: new FormControl('', Validators.required)
+  });
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    console.warn("here")
+    console.warn(this.resetPasswordForm.value)
+    console.warn(this.resetPasswordForm.value)
+    
   }
 
 }
