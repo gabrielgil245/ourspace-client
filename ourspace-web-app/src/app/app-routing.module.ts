0973859bef7b74b1fc646028c5a8a128909d48c4
gabrielgil245 <gabrielgil245@gmail.com>
2021-09-07 16:10:43 -0500

    initial pages setup; skeleton navbar component; bootstrap styling added

--- ourspace-web-app/src/app/app-routing.module.ts
+++ ourspace-web-app/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
+import { NewPasswordComponent } from './pages/new-password/new-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent },
+  {path: 'signup', component: SignUpComponent},
+  {path: 'reset-password', component: ResetPasswordComponent},
+  {path: 'new-password', component: NewPasswordComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ourspace-web-app/src/app/app.module.ts
+++ ourspace-web-app/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './pages/login/login.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
+import { NewPasswordComponent } from './pages/new-password/new-password.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignUpComponent,
+    ResetPasswordComponent,
+    NewPasswordComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
