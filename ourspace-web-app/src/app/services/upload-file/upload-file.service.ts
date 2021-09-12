ea32f23702f9f0829f15fc3223da777cb688f83d
Jack Gildea <jack.k.gildea@gmail.com>
2021-09-12 11:06:18 -0400

    I opened on wrong branch, I don't think I even made any changes

--- ourspace-web-app/src/app/services/upload-file/upload-file.service.ts
+++ ourspace-web-app/src/app/services/upload-file/upload-file.service.ts
@@ -16,7 +16,7 @@ export class UploadFileService {
       const uploadImageData = new FormData();
           uploadImageData.append('imageFile', selectedFile, selectedFile.name);
           //Make a call to the Spring Boot Application to save the image
-          this.httpClient.post('http://localhost:9000/api/signup', uploadImageData, { observe: 'response' })
+          this.httpClient.post('http://localhost:9000/S3/signup', uploadImageData, { observe: 'response' })
             .subscribe((response) => {
               if (response.status === 200) {
                 this.message = 'Image uploaded successfully';
