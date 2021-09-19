0fabb6a009ba550f375beb08c67263d37a6c19a1
gabrielgil245 <gabrielgil245@gmail.com>
2021-09-18 23:29:35 -0500

    Attempted to test user services, but was unsuccessful

--- ourspace-web-app/src/app/services/user.service.spec.ts
+++ ourspace-web-app/src/app/services/user.service.spec.ts
@@ -1,3 +1,5 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DebugElement } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
@@ -5,12 +7,56 @@ import { UserService } from './user.service';
 describe('UserService', () => {
   let service: UserService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UserService]
+    });
     service = TestBed.inject(UserService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should login the user', async () => {
+    expect(service.userLogin).toBeTruthy();
+  })
+
+  it('should get list of users', async () => {
+    expect(service.getListOfUser).toBeTruthy();
   });
+
+  it('should get userinfo by email', async () => {
+    expect(service.getUserByEmail).toBeTruthy();
+  })
+
+  it('should get userinfo by username', async () => {
+    expect(service.getUserByUsername).toBeTruthy();
+  })
+
+  it('should get userinfo by username', async () => {
+    expect(service.getUserByUsername).toBeTruthy();
+  })
+
+  it('should check user login session', async () => {
+    expect(service.checkSession).toBeTruthy();
+  })
+
+  it('should log out the user', async () => {
+    expect(service.logout).toBeTruthy();
+  })
+
+  it('should create a new user', async () => {
+    expect(service.createNew).toBeTruthy();
+  })
+
+  it('should reset user password', async () => {
+    expect(service.resetPassword).toBeTruthy();
+  })
+
+  it('should send a forgot user password email', async () => {
+    expect(service.forgotPassword).toBeTruthy();
+  })
+
+  it('should edit user info', async () => {
+    expect(service.editProfile).toBeTruthy();
+  })
+  
 });
